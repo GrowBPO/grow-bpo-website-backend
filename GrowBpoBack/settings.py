a853c6acc5e941d59a253143b3b6dd358010ece9
TThaynara Martins <thaynararm2@hotmail.com>
2024-06-16 22:22:14 +0000

    BUILD: Mudança de diretórios

--- GrowBpoBack/settings.py
+++ GrowBpoBack/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/django_static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'GrowBpoBack/static')
+    os.path.join(BASE_DIR, 'GrowBpoBack/django_static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'django_static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
