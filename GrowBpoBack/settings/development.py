54a18c4669e9cde912a29db31136c8985e7df4d9
Thay <thaynararm2@hotmail.com>
2024-06-26 17:02:46 -0300

    FIX: Correções settings

--- GrowBpoBack/settings/development.py
+++ GrowBpoBack/settings/development.py
@@ -7,12 +7,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DEVELOPMENT_DB_NAME'),
-        'USER': os.getenv('DEVELOPMENT_DB_USER'),
-        'PASSWORD': os.getenv('DEVELOPMENT_DB_PASSWORD'),
-        'HOST': os.getenv('DEVELOPMENT_DB_HOST'),
-        'PORT': os.getenv('DEVELOPMENT_DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
